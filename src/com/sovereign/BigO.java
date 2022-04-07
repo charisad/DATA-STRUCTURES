66c59d9d7e0008ebc57300d188aa37414be90e06
Collins Adom Baffour <adombaffourcollins@gmail.com>
2022-04-07 23:59:14 +0000

    Other Algos

--- src/com/sovereign/BigO.java
+++ src/com/sovereign/BigO.java
@@ -1,5 +1,32 @@
 package com.sovereign;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
 public class BigO {
 
-}
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            double payment = scanner.nextDouble();
+            scanner.close();
+
+            // Write your code here.
+
+            NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+            NumberFormat nu = NumberFormat.getCurrencyInstance( Locale.US);
+            NumberFormat nc = NumberFormat.getCurrencyInstance(Locale.CHINA);
+            NumberFormat ni = NumberFormat.getCurrencyInstance(new Locale("en","IN"));
+
+            System.out.println("US: " + nu.format(payment));
+            System.out.println("India: " + ni.format(payment));
+            System.out.println("China: " + nc.format(payment));
+            System.out.println("France: " + nf.format(payment));
+        }
+    }
