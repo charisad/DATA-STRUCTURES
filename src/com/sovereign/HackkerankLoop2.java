4cca8f44c2c99f7dbb8be728278b928587378175
Collins Adom Baffour <adombaffourcollins@gmail.com>
2022-04-09 07:47:27 +0000

    Other Algos

--- src/com/sovereign/HackkerankLoop2.java
+++ src/com/sovereign/HackkerankLoop2.java
@@ -6,12 +6,12 @@ public class HackkerankLoop2 {
 
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
+        int t=1;
 
         for(int i=0;i<t;i++){
-            int  a = in.nextInt();
-            int  b = in.nextInt();
-            int  n = in.nextInt();
+            int  a = 5;
+            int  b = 3;
+            int  n = 5;
 
             for(int j=1;j<=n;j++){
                 int k=0;
